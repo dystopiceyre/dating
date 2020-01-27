55cf77cf068ef5d44053e483affa97621403b410
dystopiceyre <livvy16.olivia@gmail.com>
2020-01-26 22:22:34 -0800

    Style tweaks and filled in content for the three pages

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+//Start a session
+session_start();
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -10,9 +13,14 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Run fat free
-$f3->route('GET /', function() {
+$f3->route('GET /', function () {
     $view = new Template();
     echo $view->render('views/home.html');
-}
-);
+});
+
+//define a personal route
+
+//define a profile route
+
+//define an interests route
 $f3->run();
