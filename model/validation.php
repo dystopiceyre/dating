7ba070f5470fd19e9a0a07024a45f1d86418c5a9
dystopiceyre <livvy16.olivia@gmail.com>
2020-02-05 15:42:05 -0800

    Set up data validation

--- model/validation.php
+++ model/validation.php
@@ -1,22 +1,44 @@
 <?php
-//TODO: validation
 /*
- *
-validName()
-checks to see that a string is all alphabetic
-validAge()
-checks to see that an age is numeric and between 18 and 118
-validPhone()
-checks to see that a phone number is valid (you can decide what constitutes a “valid” phone number)
-validEmail()
-checks to see that an email address is valid
-validOutdoor()
-checks each selected outdoor interest against a list of valid options
-validIndoor()
-checks each selected indoor interest against a list of valid options
 Make name, age, phone, and email required fields
 Gender, bio, and interests are optional.
 In your controller, require the validation file. When each form is submitted, validate the
 data in that form using the appropriate functions. If there are no errors, then store the
 data in session variables and display the next form
  */
+
+//checks to see that a string is all alphabetic
+function validName()
+{
+
+}
+
+//checks to see that an age is numeric and between 18 and 118
+function validAge($age)
+{
+   return !empty($age) && ctype_digit($age) && $age >= 18 && $age <= 118;
+}
+
+//checks to see that a phone number is valid
+function validPhone()
+{
+
+}
+
+//checks to see that an email address is valid
+function validEmail()
+{
+
+}
+
+//checks each selected indoor interest against a list of valid options
+function validIndoor()
+{
+
+}
+
+//checks each selected outdoor interest against a list of valid options
+function validOutdoor()
+{
+
+}
