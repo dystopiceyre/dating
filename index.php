55c941b60e6519b48e0c2c5dc47da69f8d81b018
dystopiceyre <livvy16.olivia@gmail.com>
2020-02-05 14:55:12 -0800

    Changed folder name from views to view, fixed issue with putting first and last name in session variables

--- index.php
+++ index.php
@@ -11,45 +11,46 @@ require_once('vendor/autoload.php');
 //Create instance of base class
 $f3 = Base::instance();
 
-//Run fat free
+//define default route
 $f3->route('GET /', function () {
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('view/home.html');
 });
 
 //define a personal route
 $f3->route('GET /personal', function () {
     $view = new Template();
-    echo $view->render('views/personal.html');
+    echo $view->render('view/personal.html');
 });
 
 //define a profile route
 $f3->route('POST /profile', function () {
-    $_SESSION['first-name'] = $_POST['first-name'];
-    $_SESSION['last-name'] = $_POST['last-name'];
+    $_SESSION['firstName'] = $_POST['firstName'];
+    $_SESSION['lastName'] = $_POST['lastName'];
     $_SESSION['age'] = $_POST['age'];
     $_SESSION['gender'] = $_POST['gender'];
     $_SESSION['phone-number'] = $_POST['phone-number'];
     $view = new Template();
-    echo $view->render('views/profile.html');
+    echo $view->render('view/profile.html');
 });
 
 //define an interests route
 $f3->route('POST /interests', function () {
     $_SESSION['email'] = $_POST['email'];
     $_SESSION['state'] = $_POST['state'];
-    $_SESSION['seeking[]'] = $_POST['seeking[]'];
+    $_SESSION['seeking'] = $_POST['seeking'];
     $_SESSION['bio'] = $_POST['bio'];
     $view = new Template();
-    echo $view->render('views/interests.html');
+    echo $view->render('view/interests.html');
 });
 
 //define a summary route
 $f3->route('POST /summary', function () {
-    $_SESSION['indoor-interests[]'] = $_POST['indoor-interests[]'];
-    $_SESSION['outdoor-interests[]'] = $_POST['outdoor-interests[]'];
+    $_SESSION['indoor-interests'] = $_POST['indoor-interests'];
+    $_SESSION['outdoor-interests'] = $_POST['outdoor-interests'];
+    var_dump($_SESSION);
     $view = new Template();
-    echo $view->render('views/summary.html');
+    echo $view->render('view/summary.html');
 });
 
 $f3->run();
--- model/validation.php
+++ model/validation.php
@@ -1,2 +1,22 @@
 <?php
 //TODO: validation
+/*
+ *
+validName()
+checks to see that a string is all alphabetic
+validAge()
+checks to see that an age is numeric and between 18 and 118
+validPhone()
+checks to see that a phone number is valid (you can decide what constitutes a “valid” phone number)
+validEmail()
+checks to see that an email address is valid
+validOutdoor()
+checks each selected outdoor interest against a list of valid options
+validIndoor()
+checks each selected indoor interest against a list of valid options
+Make name, age, phone, and email required fields
+Gender, bio, and interests are optional.
+In your controller, require the validation file. When each form is submitted, validate the
+data in that form using the appropriate functions. If there are no errors, then store the
+data in session variables and display the next form
+ */
