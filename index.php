ceb2d86b4c9e43bade96cf2f399dfc180661d123
dystopiceyre <livvy16.olivia@gmail.com>
2020-02-09 17:43:47 -0800

    Added validation to index file

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 
 //Require once the autoload file
 require_once('vendor/autoload.php');
+require_once('model/validation.php');
 
 //Create instance of base class
 $f3 = Base::instance();
@@ -26,37 +27,104 @@ $f3->route('GET /', function () {
 });
 
 //define a personal route
-$f3->route('GET /personal', function () {
+$f3->route('GET /personal', function ($f3) {
+    //If form has been submitted, validate
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Get data from form
+        $first = $_POST['firstName'];
+        $last = $_POST['lastName'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        $phone = $_POST['phoneNumber'];
+
+        //Add data to hive
+        $f3->set('firstName', $first);
+        $f3->set('lastName', $last);
+        $f3->set('age', $age);
+        $f3->set('gender', $gender);
+        $f3->set('phoneNumber', $phone);
+
+        //If data is valid
+        if (validForm()) {
+            //Write data to Session
+            $_SESSION['firstName'] = $first;
+            $_SESSION['lastName'] = $last;
+            $_SESSION['age'] = $age;
+            $_SESSION['gender'] = $gender;
+            $_SESSION['phoneNumber'] = $phone;
+
+            //Redirect to Profile
+            $f3->reroute('/profile');
+        }
+    }
+
+    //display form
     $view = new Template();
     echo $view->render('view/personal.html');
 });
 
 //define a profile route
-$f3->route('POST /profile', function () {
-    $_SESSION['firstName'] = $_POST['firstName'];
-    $_SESSION['lastName'] = $_POST['lastName'];
-    $_SESSION['age'] = $_POST['age'];
-    $_SESSION['gender'] = $_POST['gender'];
-    $_SESSION['phoneNumber'] = $_POST['phoneNumber'];
+$f3->route('POST /profile', function ($f3) {
+    $selectedSeeking = array();
+    $email = $_POST['email'];
+    $state = $_POST['state'];
+    $bio = $_POST['bio'];
+    if (!empty($_POST['seeking'])) {
+        $selectedSeeking = $_POST['seeking'];
+    }
+
+    $f3->set('email', $email);
+    $f3->set('state', $state);
+    $f3->set('seeking', $selectedSeeking);
+
+
+    if (validForm()) {
+        //Write data to Session
+        $_SESSION['email'] = $email;
+        $_SESSION['state'] = $state;
+        $_SESSION['seeking'] = $selectedSeeking;
+        $_SESSION['bio'] = $bio;
+
+        //Redirect to Interests
+        $f3->reroute('/interests');
+    }
+
     $view = new Template();
     echo $view->render('view/profile.html');
 });
 
 //define an interests route
-$f3->route('POST /interests', function () {
-    $_SESSION['email'] = $_POST['email'];
-    $_SESSION['state'] = $_POST['state'];
-    $_SESSION['seeking'] = $_POST['seeking'];
-    $_SESSION['bio'] = $_POST['bio'];
+$f3->route('POST /interests', function ($f3) {
+    $selectedIndoor = array();
+    $selectedOutdoor = array();
+
+    if (!empty($_POST['indoorInterests'])) {
+        $selectedIndoor = $_POST['indoorInterests'];
+    }
+    if (!empty($_POST['outdoorInterests'])) {
+        $selectedOutdoor = $_POST['outdoorInterests'];
+    }
+
+    $f3->set('indoorInterests', $selectedIndoor);
+    $f3->set('outdoorInterests', $selectedOutdoor);
+
+
+    if (validForm()) {
+        //Write data to Session
+        $_SESSION['indoorInterests'] = $selectedIndoor;
+        $_SESSION['outdoorInterests'] = $selectedOutdoor;
+
+        //Redirect to Summary
+        $f3->reroute('/summary');
+    }
+
     $view = new Template();
     echo $view->render('view/interests.html');
 });
 
 //define a summary route
 $f3->route('POST /summary', function () {
-    $_SESSION['indoorInterests'] = $_POST['indoorInterests'];
-    $_SESSION['outdoorInterests'] = $_POST['outdoorInterests'];
-    $view = new Template();
+       $view = new Template();
     echo $view->render('view/summary.html');
 });
 
--- model/validation.php
+++ model/validation.php
@@ -1,11 +1,13 @@
 <?php
-/*
-Make name, age, phone, and email required fields
-Gender, bio, and interests are optional.
-In your controller, require the validation file. When each form is submitted, validate the
-data in that form using the appropriate functions. If there are no errors, then store the
-data in session variables and display the next form
+/* Validate the form
+ * @return boolean
  */
+function validForm()
+{
+    global $f3;
+    $isValid = true;
+    return $isValid;
+}
 
 //checks to see that a string is all alphabetic
 function validName($name)
@@ -22,21 +24,24 @@ function validAge($age)
 //checks to see that a phone number is valid
 function validPhone($phone)
 {
-    return !empty($phone);
+    return !empty($phone) && preg_match('\/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/',
+            $phone);
 }
 
 //checks to see that an email address is valid
+//regex pattern from: https://www.regular-expressions.info/email.html
 function validEmail($email)
 {
-    return !empty($email);
+    return !empty($email) && preg_match('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:
+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?', $email);
 }
 
 //checks each selected seeking value against a list of valid options
-function validateSeeking($seeking) {
+function validateSeeking($seeking)
+{
     global $f3;
     return in_array($seeking, $f3->get('seeking'));
 }
-
 
 //checks each selected indoor interest against a list of valid options
 function validIndoor($indoor)
