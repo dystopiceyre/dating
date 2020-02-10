44c8b8fd84940e8aa9a069c0fd2a2bbf9a62db56
dystopiceyre <livvy16.olivia@gmail.com>
2020-02-09 18:48:05 -0800

    Added validForm function to validation file

--- model/validation.php
+++ model/validation.php
@@ -6,13 +6,68 @@ function validForm()
 {
     global $f3;
     $isValid = true;
+
+    if (!validFirstName($f3->get('firstName'))) {
+
+        $isValid = false;
+        $f3->set("errors['firstName']", "Please enter a first name");
+    }
+
+    if (!validLastName($f3->get('lastName'))) {
+
+        $isValid = false;
+        $f3->set("errors['lastName']", "Please enter a last name");
+    }
+
+    if (!validAge($f3->get('age'))) {
+
+        $isValid = false;
+        $f3->set("errors['age']", "Please enter an age between 18 and 118");
+    }
+
+    if (!validPhone($f3->get('phoneNumber'))) {
+
+        $isValid = false;
+        $f3->set("errors['phoneNumber']", "Please enter a phone number");
+    }
+
+    if (!validEmail($f3->get('email'))) {
+
+        $isValid = false;
+        $f3->set("errors['email']", "Please enter a valid email address");
+    }
+
+    if (!validSeeking($f3->get('seeking'))) {
+
+        $isValid = false;
+        $f3->set("errors['seeking']", "Invalid selection");
+    }
+
+    if (!validIndoor($f3->get('indoorInterests'))) {
+
+        $isValid = false;
+        $f3->set("errors['indoorInterests']", "Invalid selection");
+    }
+
+    if (!validOutdoor($f3->get('outdoorInterests'))) {
+
+        $isValid = false;
+        $f3->set("errors['outdoorInterests']", "Invalid selection");
+    }
+
     return $isValid;
 }
 
 //checks to see that a string is all alphabetic
-function validName($name)
+function validFirstName($first)
 {
-    return !empty($name) && ctype_alpha($name);
+    return !empty($first) && ctype_alpha($first);
+}
+
+//checks to see that a string is all alphabetic
+function validLastName($last)
+{
+    return !empty($last) && ctype_alpha($last);
 }
 
 //checks to see that an age is numeric and between 18 and 118
@@ -22,9 +77,10 @@ function validAge($age)
 }
 
 //checks to see that a phone number is valid
+//regex pattern from http://regexlib.com/REDetails.aspx?regexp_id=607
 function validPhone($phone)
 {
-    return !empty($phone) && preg_match('\/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/',
+    return !empty($phone) && preg_match('^(?:\([2-9]\d{2}\)\ ?|[2-9]\d{2}(?:\-?|\ ?))[2-9]\d{2}[- ]?\d{4}$',
             $phone);
 }
 
@@ -32,12 +88,11 @@ function validPhone($phone)
 //regex pattern from: https://www.regular-expressions.info/email.html
 function validEmail($email)
 {
-    return !empty($email) && preg_match('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:
-[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?', $email);
+    return !empty($email) && preg_match('\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', $email);
 }
 
 //checks each selected seeking value against a list of valid options
-function validateSeeking($seeking)
+function validSeeking($seeking)
 {
     global $f3;
     return in_array($seeking, $f3->get('seeking'));
