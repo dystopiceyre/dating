a9c604e254884f5551532ec7f86e3abc5e7255ce
dystopiceyre <livvy16.olivia@gmail.com>
2020-02-26 17:48:09 -0800

    remove "connected echo"

---
+++ model/datingDatabase.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class DatingDatabase
+{
+    function __construct()
+    {
+    }
+
+    function connect()
+    {
+
+    }
+
+    function getMembers()
+    {
+    }
+
+    function getMember($member_id)
+    {
+
+    }
+
+    function getInterests($member_id)
+    {
+
+    }
+}
