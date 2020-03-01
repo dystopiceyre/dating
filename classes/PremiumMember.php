fd68c33366a4fba5f411ff2999d6f3c5683590f2
dystopiceyre <livvy16.olivia@gmail.com>
2020-02-29 20:05:20 -0800

    added image to Premium constructor, along with getters and setters

--- classes/PremiumMember.php
+++ classes/PremiumMember.php
@@ -4,13 +4,17 @@ class PremiumMember extends Member
 {
     private $_indoorInterests;
     private $_outdoorInterests;
+    private $_image;
 
-   /*
-    * PremiumMember constructor calls it's parent class' constructor
-    */
-    public function __construct($fname, $lname, $age, $gender, $phone)
+    /*
+     * PremiumMember constructor calls it's parent class' constructor
+     */
+    public function __construct($fname, $lname, $age, $gender, $phone, $indoorInterests, $outdoorInterests, $image = NULL)
     {
         parent::__construct($fname, $lname, $age, $gender, $phone);
+        $this->_indoorInterests = $indoorInterests;
+        $this->_outdoorInterests = $outdoorInterests;
+        $this->_image = $image;
     }
 
     /**
@@ -43,5 +47,21 @@ class PremiumMember extends Member
     public function setOutdoorInterests($outdoorInterests)
     {
         $this->_outdoorInterests = $outdoorInterests;
+    }
+
+    /**
+     * @return null
+     */
+    public function getImage()
+    {
+        return $this->_image;
+    }
+
+    /**
+     * @param null $image
+     */
+    public function setImage($image)
+    {
+        $this->_image = $image;
     }
 }
